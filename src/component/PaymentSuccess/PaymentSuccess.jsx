4cf990b8571250f19929340b9e251e88de146545
Empire688682 <asehindej@gmail.com>
2025-05-04 22:07:37 +0100

    useRouter

--- src/component/PaymentSuccess/PaymentSuccess.jsx
+++ src/component/PaymentSuccess/PaymentSuccess.jsx
@@ -1,6 +1,6 @@
 "useclient";
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function PaymentSuccess() {
   const router = useRouter();
