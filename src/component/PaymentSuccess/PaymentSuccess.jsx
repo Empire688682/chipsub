c60141228c07b98692ba001df8d004733c61f67e
Empire688682 <asehindej@gmail.com>
2025-05-05 23:01:50 +0100

    found

--- src/component/PaymentSuccess/PaymentSuccess.jsx
+++ src/component/PaymentSuccess/PaymentSuccess.jsx
@@ -8,10 +8,12 @@ export default function PaymentSuccess() {
   const searchParams = useSearchParams();
   const transaction_id = searchParams.get('transaction_id');
   const [status, setStatus] = useState('Verifying payment...');
-  const {route} = useGlobalContext();
+  const { route } = useGlobalContext();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (transaction_id) {
+      setLoading(true);
       fetch('/api/verify-payment', {
         method: 'POST',
         headers: {
@@ -22,22 +24,38 @@ export default function PaymentSuccess() {
         .then(res => res.json())
         .then(data => {
           if (data.success) {
-            setStatus(data.message);
+            setStatus("✅ " + data.message);
           } else {
             setStatus('❌ Verification failed');
           }
         })
-        .catch(() => setStatus('❌ Error verifying payment'));
+        .catch(() => setStatus('❌ Error verifying payment'))
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, [transaction_id]);
 
+
   return (
     <div className="flex items-center justify-center gap-6 flex-col">
-      <div className="bg-white p-6 rounded-2xl shadow-md text-center max-w-md">
+      <div className="bg-white p-6 rounded-2xl shadow-md flex gap-4 flex-col text-center max-w-md">
         <h2 className="text-2xl font-semibold text-gray-800">{status}</h2>
-        <p>Transaction ID: {transaction_id}</p>
+        {loading && (
+          <div className="flex justify-center items-center mt-4">
+            <div className="w-6 h-6 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+            <span className="ml-2 text-gray-600">Verifying...</span>
+          </div>
+        )}
+        {
+          loading ?
+            <p>Please wait while we verify your payment</p>
+            :
+            <p>Verification process complete</p>
+        }
+        <p className="text-gray-500 text-sm">Transaction ID: <span className='font-semibold'>{transaction_id}</span></p>
       </div>
-      <button onClick={()=>route.push("/dashboard")} className='bg-blue-600 text-white px-4 py-2 rounded-lg text-sm hover:bg-blue-700 transition'>To Dashboard</button>
+      <button onClick={() => route.push("/dashboard")} className='bg-blue-600 text-white px-4 py-2 rounded-lg text-sm hover:bg-blue-700 transition'>To Dashboard</button>
     </div>
   );
 }
