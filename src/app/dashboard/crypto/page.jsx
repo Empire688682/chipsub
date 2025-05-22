3e0fbd2580384da883f3039133af841c663fd151
juwon <asehindej@gmail.com>
2025-05-22 20:37:15 +0100

    crypto

---
+++ src/app/dashboard/crypto/page.jsx
@@ -0,0 +1,12 @@
+import Crypto from '@/component/Crypto/Crypto'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='px-6 py-12'>
+      <Crypto />
+    </div>
+  )
+}
+
+export default page
